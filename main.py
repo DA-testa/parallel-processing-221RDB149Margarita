312708c5b8affeeecded3de279e3bc94d7b67fef
Margarita <124924807+221RDB149Margarita@users.noreply.github.com>
2023-03-19 00:02:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import heapq
 n, m = map(int, input().split())
 times = list(map(int, input().split()))
 
-# Initialize the heap with the starting times of each thread
+
 threads = [(0, i) for i in range(n)]
 
-# Process each job
 for i in range(m):
-    # Get the next job time from the input
+
     job_time = times[i]
-    # Get the next available thread from the heap
+
     start_time, thread_id = heapq.heappop(threads)
-    # Output the thread and start time for this job
+
     print(thread_id, start_time)
-    # Update the start time for this thread
+
     start_time += job_time
-    # Add the thread back to the heap with its updated start time
+
     heapq.heappush(threads, (start_time, thread_id))
